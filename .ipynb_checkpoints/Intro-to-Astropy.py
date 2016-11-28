cd5d9dcaf2faf39c571e6380d673d8e4d79120cd
King <n00787683@ospreys.unf.edu>
2016-11-28 14:33:54 -0500

    Commiting stuff

---
+++ .ipynb_checkpoints/Intro-to-Astropy.py
@@ -0,0 +1,29 @@
+
+# coding: utf-8
+
+# In[1]:
+
+# Import system packages.
+from __future__ import absolute_import, division, print_function
+import sys
+import warnings
+# Import installed packages.
+import numpy as np
+import scipy.constants as sci_con
+import astropy.constants as ast_con
+import binstarsolver as bss
+# IPython magic.
+get_ipython().magic('matplotlib inline')
+
+
+# In[1]:
+
+import astropy
+
+
+# In[1]:
+
+import skyview
+
+
+# In[ ]:
